f753736c3a1a4c1a000988fd898425d5c846f3a2
tarcisioaraujo <tarcisio.araujo@gafit.com.br>
2022-04-04 16:43:55 -0300

    created 2 new routes

--- first-laravel-project/routes/web.php
+++ first-laravel-project/routes/web.php
@@ -16,3 +16,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/contatos', function () {
+    return view('contact');
+});
+Route::get('/sobre', function () {
+    return view('about');
+});
